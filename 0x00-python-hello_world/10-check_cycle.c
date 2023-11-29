59be908bf5a9c42053c3de93215e524268c377fc
Lawremwashail <lawremwash@gmail.com>
2023-11-29 13:41:00 +0000

    list

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks the cycle in list
+ * @list: list to check
+ * Return: 1 for cycle, 0 if no cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *fast, *slow;
+
+	if (!list || !list->next)
+		return (0);
+	fast = list;
+	slow = list;
+
+	while (slow != NULL && fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
