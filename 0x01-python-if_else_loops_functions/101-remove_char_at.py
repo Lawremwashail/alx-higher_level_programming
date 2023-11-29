da079ea2eac365ae802a2d99286f5288f5a1445b
Lawremwashail <lawremwash@gmail.com>
2023-11-29 11:34:57 +0000

    remove_char

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    new_char = ""
+    new_char = ''
     i = 0
     for char in str:
         if i != n:
             new_char += str[0]
-
         i++
     return (new_char)
