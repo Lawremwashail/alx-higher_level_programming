8d051bebb050496b53a024c81d5fb90965afe9f0
Lawremwashail <lawremwash@gmail.com>
2023-11-30 15:53:27 +0000

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,6 +9,6 @@ if __name__ == "__main__":
     elif num_count == 1:
         print("1 arg")
     else:
-        print("{} args:".format(count))
+        print("{} args:".format(num_count))
     for i in range(num_count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
