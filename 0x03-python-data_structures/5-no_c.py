518c52d9cb075fdb6d304556e42cafda4d235f14
Lawremwashail <lawremwash@gmail.com>
2023-12-03 17:37:36 +0000

    noc

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3i
 def no_c(my_string):
-    if my_string = "c" and my_string == "C":
-        return my_string.remove()
-    my_new_string = my_string.remove()
-    return my_new_string
+
+    my_new_string = ""
+
+    for character in my_string:
+        if character.lower() != 'c':
+            my_new_string += character
+        return my_new_string
