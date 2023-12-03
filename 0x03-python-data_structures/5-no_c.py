466b84771c21509102b05eaa115f6dfa2940b6ad
Lawremwashail <lawremwash@gmail.com>
2023-12-03 17:30:17 +0000

    noc

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3i
+def no_c(my_string):
+    if my_string = "c" and my_string == "C":
+        return my_string.remove()
+    my_new_string = my_string.remove()
+    return my_new_string
