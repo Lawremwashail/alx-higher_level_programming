60de6872ac76f8dddf6127f892f24075b9ed99c0
Lawremwashail <lawremwash@gmail.com>
2023-12-06 12:25:18 +0000

    multy

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    return {keys: val * 2 for keys, value in a_dictionary.items()}
+    return {keys: val * 2 for keys, val in a_dictionary.items()}
