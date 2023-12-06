7d721f50475715ebe7905489dd3ef0b914ebf2cc
Lawremwashail <lawremwash@gmail.com>
2023-12-06 11:37:37 +0000

    search

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_new_list = list(map(lambda n: replace if n == search else n, mylist))
-    return (my_new_list)
+    my_new_list = list(map(lambda e: replace if e == search else e, mylist))
+    return my_new_list
