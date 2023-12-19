cfc688c474827e31da21bbe0dd7ec70a2c5172ff
Lawremwashail <lawremwash@gmail.com>
2023-12-19 16:57:35 +0000

    2sq

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""Module for square"""
 
 
 class Square:
+    """Represents a square"""
     def __init__(self, size=0):
+        """Initialization for square
+        Args:
+            size (int): The size of the square
+        """
         if not isinstance(size, int):
+            """Checks whether size is of type int and raises error if not"""
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
