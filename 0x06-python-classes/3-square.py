d6438ba1fd1f99bc408089f657d716aee1ef7c6a
Lawremwashail <lawremwash@gmail.com>
2023-12-19 17:27:59 +0000

    3sqre

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""Class module"""
 
 
 class Square:
+    """Defines a class"""
     def __init__(self, size=0):
+        """
+        Initialization of square and size
+        Args:
+            size (int): The size of one side
+        Raises:
+            TypeError if size is not an integer
+            ValueError if value is less than 0
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -10,4 +20,11 @@ class Square:
         self.__size = size
 
     def area(self):
+        """
+        Area of the square.
+
+        Returns:
+            The Area of the sqaure.
+        """
+
         return (self.__size * self.__size)
