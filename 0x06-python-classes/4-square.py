3fc38ecab31fa8f10a402c170c32dcc0ddf64f94
Lawremwashail <lawremwash@gmail.com>
2023-12-19 17:36:59 +0000

    4_square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
+"""Class Module"""
 
 
 class Square:
+    """Represents a square"""
     def __init__(self, size=0):
+        """"
+        Initialization for square size
+        Args:
+            size (int): The size of one side of square
+        """
         self.size = size
 
     @property
     def size(self):
+        """
+        Get or set the current size of the square
+        Returns:
+            TypeError: if size is not an integer
+            ValueError: if value is less than 0
+        """
         return (self.__size)
 
     @size.setter
@@ -18,4 +31,5 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Returns the Area of the square"""
         return (self.__size ** 2)
