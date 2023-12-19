49641dc241c9e1c1d63aafdce3a63a562836cf1d
Lawremwashail <lawremwash@gmail.com>
2023-12-19 13:17:23 +0000

    0_square

--- 0x06-python-classes/0_square.py
+++ 0x06-python-classes/0_square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-'''Defination of class square.'''
+
+"""Defination of class square."""
 
 
 class Square:
-    '''represents the square class'''
+    """represents the square class."""
     pass
