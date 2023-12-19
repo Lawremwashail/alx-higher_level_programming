b30668b6d09da0f6bdf94ab3fee5d7ba4bdaadfe
Lawremwashail <lawremwash@gmail.com>
2023-12-19 13:25:12 +0000

    square

---
+++ 0x06-python-classes/1_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defination of the square class."""
+
+
+class Square:
+    """representation of square"""
+    def __init__(self, size):
+        """Initialization of the square class
+        Args:
+            size: the size of the square in int
+        """
+        self.__size = size
