489381e736c1fbbbaf4c1f28801fd8d7df805537
Lawremwashail <lawremwash@gmail.com>
2024-01-06 11:14:26 +0000

    9threctangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -93,5 +93,6 @@ class Rectangle:
         else:
             return rect_2
 
+    @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        return cls(width=size, height=size)
