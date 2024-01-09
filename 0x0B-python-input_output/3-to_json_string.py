3d75df8717d068de126af395ee0d9ff5cb421afa
Lawremwashail <lawremwash@gmail.com>
2024-01-09 10:36:15 +0000

    json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function to return JSON string representation"""
+
+
+def to_json_string(my_obj):
+    """Return for JSON string represenatytion"""
+    return json.dumps(my_obj)
