191039fcb87546aba8d642cac9168621d225e819
Lawremwashail <lawremwash@gmail.com>
2024-01-09 11:07:34 +0000

    saveJson

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function for JSON file-writing"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """oject writing to text file with JSON"""
+    with open(filename, 'w' encode='utf8') as file:
+        return json.dump(my_obj, file)
