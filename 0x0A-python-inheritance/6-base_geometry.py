4c6cb5d2b5fbcff652002eadd418950cbbeda90c
Lawremwashail <lawremwash@gmail.com>
2024-01-10 14:05:57 +0000

    basegeo

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Represents a class"""
+
+
+class BaseGeometry:
+    """Defines the class"""
+    def area(self):
+        """Function for area"""
+        raise Exception("area() is not implemented")
