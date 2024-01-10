6e8550d36c646c62361a40a68ad722d85faf36ef
Lawremwashail <lawremwash@gmail.com>
2024-01-10 13:21:39 +0000

    mylist

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Represents a subclass myList"""
+
+
+class myList(List):
+    """class that inherits from the class List"""
+
+    def print_sorted(self):
+        """prints lists in a sorted order"""
+        print(sorted(self))
