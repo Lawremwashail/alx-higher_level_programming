501e80482c75bba601d26bd17a1b76947034a837
Lawremwashail <lawremwash@gmail.com>
2024-04-09 18:26:13 +0000

    6square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,11 +2,13 @@
 const BaseSquare = require('./5-square.js');
 
 module.exports = class Square extends BaseSquare {
-  charPrint (c) {
+  charPrint(c) {
     if (c === undefined) {
       this.print();
     } else {
-        for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
 };
