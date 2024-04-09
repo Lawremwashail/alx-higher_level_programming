83a18cb01cb32ac370eabf1292879b1c9fa07216
Lawremwashail <lawremwash@gmail.com>
2024-04-09 15:04:11 +0000

    3rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 module.exports = class Rectangle {
 	constructor (w, h) {
 		if ((w > 0) && (h > 0)) {
@@ -15,4 +16,4 @@ module.exports = class Rectangle {
 			console.log(k);
 		}
 	}
-}
+};
