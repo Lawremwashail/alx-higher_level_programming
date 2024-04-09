1b8557df9745ebea969e320609260c474ca6e07a
Lawremwashail <lawremwash@gmail.com>
2024-04-09 15:27:03 +0000

    5square

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-module.exports = class Square extends rectangle require('./4-rectangle.js') {
+module.exports = class Square extends Rectangle require('./4-rectangle.js') {
 	constructor (size) {
 		super(size, size);
 	}
