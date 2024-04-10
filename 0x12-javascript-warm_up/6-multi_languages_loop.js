500fa214a392b481d468b088396d601d5a2882f8
Lawremwashail <lawremwash@gmail.com>
2024-04-10 11:12:07 +0000

    6-multi

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const myVars = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myLangs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < 3; i++) {
-  console.log(myVars[i]);
+  console.log(myLangs[i]);
 }
