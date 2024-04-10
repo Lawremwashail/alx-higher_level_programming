36ca20428e9cee2124a225005afa3eeb8020af39
Lawremwashail <lawremwash@gmail.com>
2024-04-10 10:22:43 +0000

    5to-integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const arg = process.argv[2];
+
+if (!isNaN(arg) && arg !== undefined) {
+    const number = parseInt(arg);
+    console.log('My number:', number);
+} else {
+    console.log('Not a number');
+}
