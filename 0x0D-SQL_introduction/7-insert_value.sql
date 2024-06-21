c69de49e35399dc390ee975a8aaa361f0f42c4a0
Lawremwashail <lawremwash@gmail.com>
2024-06-21 17:12:13 +0000

    insert_into

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
 -- inserts a row on first_table
-INSERT INTO first_table (id, name) VALUES (89 'Best School');
+-- values in the rows are id of 89 and name is Best School
+INSERT INTO first_table ('id', 'name') VALUES (89 'Best School');
