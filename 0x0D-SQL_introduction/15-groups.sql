1f872e2de3d688b7c9ab063aa39cb595075c6d5d
Lawremwashail <lawremwash@gmail.com>
2024-06-21 19:23:28 +0000

    scoregroup

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,4 @@
 --lists records with same score from second_table
-SELECT score, COUNT(1) AS number
-FROM second_table
+SELECT score, COUNT(*) AS number FROM second_table
 GROUP BY score
 ORDER BY number DESC;
